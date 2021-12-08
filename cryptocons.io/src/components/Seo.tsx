0ccf3af5d1b4988fe374d77421a26eba9e9378c4
rossmoody <fiftyfivehis@gmail.com>
2021-12-07 19:15:14 -0800

    JK

--- cryptocons.io/src/components/Seo.tsx
+++ cryptocons.io/src/components/Seo.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { Helmet } from 'react-helmet'
-import { withPrefix } from 'gatsby-link'
 import useSiteMetadata from '../hooks/use-site-metadata'
 
 type Props = {
@@ -38,18 +37,18 @@ const SEO = ({ title, description, pathname, image, children }: Props) => {
         >
             <html lang={siteLanguage} />
             <meta name="description" content={seo.description} />
-            <meta name="image" content={withPrefix(seo.image)} />
+            <meta name="image" content={seo.image} />
             <meta property="og:title" content={seo.title} />
             <meta property="og:url" content={seo.url} />
             <meta property="og:description" content={seo.description} />
-            <meta property="og:image" content={withPrefix(seo.image)} />
+            <meta property="og:image" content={seo.image} />
             <meta property="og:type" content="website" />
             <meta property="og:image:alt" content={seo.description} />
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:title" content={seo.title} />
             <meta name="twitter:url" content={seo.url} />
             <meta name="twitter:description" content={seo.description} />
-            <meta name="twitter:image" content={withPrefix(seo.image)} />
+            <meta name="twitter:image" content={seo.image} />
             <meta name="twitter:image:alt" content={seo.description} />
             <meta name="twitter:creator" content={author} />
             {children}
