3f1aa674d2412a16abd3ef963c23b322bf5645bd
rossmoody <fiftyfivehis@gmail.com>
2021-12-18 17:56:46 -0800

    Updates

--- cryptocons.io/app/root.tsx
+++ cryptocons.io/app/root.tsx
@@ -1,32 +1,56 @@
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration
-} from "remix";
-import type { MetaFunction } from "remix";
+    Links,
+    LiveReload,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
+} from 'remix'
+import type { MetaFunction } from 'remix'
 
-export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
-};
+const seo = {
+    title: 'Cryptocons.io',
+    description: 'Cryptocurrency logos and icons as tidy React components.',
+    url: 'https://cryptocons.io',
+    image: '/og-image.png',
+    author: '@_rossmoody',
+    website: 'website',
+}
+
+export const meta: MetaFunction = () => ({
+    viewport: 'width=device-width,initial-scale=1',
+    title: seo.title,
+    description: seo.description,
+    image: `${seo.url}${seo.image}`,
+    ['og:title']: seo.title,
+    ['og:url']: seo.url,
+    ['og:description']: seo.description,
+    ['og:image']: `${seo.url}${seo.image}`,
+    ['og:type']: seo.website,
+    ['og:image:alt']: seo.description,
+    ['twitter:card']: 'summary_large_image',
+    ['twitter:title']: seo.title,
+    ['twitter:url']: seo.url,
+    ['twitter:description']: seo.description,
+    ['twitter:image']: `${seo.url}${seo.image}`,
+    ['twitter:image:alt']: seo.description,
+    ['twitter:creator']: seo.author,
+})
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                <Outlet />
+                <ScrollRestoration />
+                <Scripts />
+                {process.env.NODE_ENV === 'development' && <LiveReload />}
+            </body>
+        </html>
+    )
 }
